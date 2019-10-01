f11bd15f7c1a1c123ad1b823b7e09d6fbdfeb9f5
Jon <jon.bianco@trustcommerce.com>
2019-09-30 17:14:19 -0700

    Initial commit

---
+++ PortNumberIdentifier/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortNumberIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Win32DeviceMgmt.DeviceInfo> devices = Win32DeviceMgmt.GetAllCOMPorts();
+            foreach(var device in devices)
+            {
+                Console.WriteLine($"DEVICE '{device.description}' ON PORT: {device.name}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# README #
+
+This an application to Configure Ingenico Devices.
+
+### What is this repository for? ###
+
+* Quick summary
+* REPO: https://github.com/web-projects/USB_COM_IDENTIFIER.git
+* Version
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### HISTORY ###
+
+* 20190930 - Initial repository.
